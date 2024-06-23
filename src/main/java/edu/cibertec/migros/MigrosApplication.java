e5733751fd900d85076244587780a545197107b0
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-06-23 06:59:36 -0500

    chore: initial basic project structure

---
+++ src/main/java/edu/cibertec/migros/MigrosApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.migros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrosApplication.class, args);
+    }
+
+}
