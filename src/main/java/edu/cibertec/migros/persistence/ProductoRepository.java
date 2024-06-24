228c46bee3a35f9b4346cda024f01f8f23ba5014
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-06-23 21:09:21 -0500

    fix: change file name ProductoCrudRespository to ProductoCrudRepository

--- src/main/java/edu/cibertec/migros/persistence/ProductoRepository.java
+++ src/main/java/edu/cibertec/migros/persistence/ProductoRepository.java
@@ -1,11 +1,11 @@
 package edu.cibertec.migros.persistence;
 
-import edu.cibertec.migros.persistence.crud.ProductoCrudRespository;
+import edu.cibertec.migros.persistence.crud.ProductoCrudRepository;
 import edu.cibertec.migros.persistence.entity.Producto;
 import java.util.List;
 
 public class ProductoRepository {
-    private ProductoCrudRespository productoCrudRespository;
+    private ProductoCrudRepository productoCrudRespository;
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRespository.findAll();
--- src/main/java/edu/cibertec/migros/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/edu/cibertec/migros/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,6 @@ package edu.cibertec.migros.persistence.crud;
 import edu.cibertec.migros.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRespository extends CrudRepository<Producto, Long> {
+public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
 
 }
