277c2ba90c66b2fb8f5a827fd6273b00c455f648
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-06-23 21:31:10 -0500

    chore: add findByIdCategoria query method

--- src/main/java/edu/cibertec/migros/persistence/ProductoRepository.java
+++ src/main/java/edu/cibertec/migros/persistence/ProductoRepository.java
@@ -5,9 +5,13 @@ import edu.cibertec.migros.persistence.entity.Producto;
 import java.util.List;
 
 public class ProductoRepository {
-    private ProductoCrudRepository productoCrudRespository;
+    private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRespository.findAll();
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(Long idCategoria) {
+        return productoCrudRepository.findByIdCategoria(idCategoria);
     }
 }
--- src/main/java/edu/cibertec/migros/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/edu/cibertec/migros/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,14 @@
 package edu.cibertec.migros.persistence.crud;
 
 import edu.cibertec.migros.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+import java.util.List;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+    //anotación opcional. Si se usa anotación ya no es necesario respetar el formato de nombre del método
+    //@Query(value="SELECT * FROM productos WHERE id_categoria = ?", nativeQuery=true)
+    //si no se usa @Query es obligatorio respetar el formato de nombre. A eso se le llama "Query methods"
+    List<Producto> findByIdCategoria(Long idCategoria);
 }
