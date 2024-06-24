113d28bb6a55c93b4e1326127208ee7f14fa07dd
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-06-23 19:52:45 -0500

    chore: add lombok dependency

--- src/main/java/edu/cibertec/migros/persistence/entity/Compra.java
+++ src/main/java/edu/cibertec/migros/persistence/entity/Compra.java
@@ -1,11 +1,13 @@
 package edu.cibertec.migros.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name="compras")
+@Data
 public class Compra {
 
     @Id
@@ -23,52 +25,4 @@ public class Compra {
 
     private String comentario;
     private String estado;
-
-    public Integer getIdCompra() {
-        return idCompra;
-    }
-
-    public void setIdCompra(Integer idCompra) {
-        this.idCompra = idCompra;
-    }
-
-    public Integer getIdCliente() {
-        return idCliente;
-    }
-
-    public void setIdCliente(Integer idCliente) {
-        this.idCliente = idCliente;
-    }
-
-    public LocalDateTime getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDateTime fecha) {
-        this.fecha = fecha;
-    }
-
-    public String getMedioPago() {
-        return medioPago;
-    }
-
-    public void setMedioPago(String medioPago) {
-        this.medioPago = medioPago;
-    }
-
-    public String getComentario() {
-        return comentario;
-    }
-
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
 }
